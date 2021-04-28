2b4fe8014cef3af477dabcf48dbdbce7eed3082f
Archit Agrawal <archit18221@iiitd.ac.in>
2021-04-28 18:16:30 +0530

    Add twitter generation

--- src/CovidAPI.py
+++ src/CovidAPI.py
@@ -35,3 +35,19 @@ def fetch_data_from_API(resource, location):
         return res
     else:
         return "All the resources are available at https://t.me/covid_iiitd"
+
+def join_entries(entries):
+    entries_url = entries[0]
+
+    for i in entries[1:]:
+        entries_url += '%20OR%20{}'.format(str(i))
+    return entries_url
+
+def get_twitter_link(cities, resources):
+    if resources == [] or cities == []:
+        return ''
+
+    location_text = join_entries(cities)
+    resources_text = join_entries(resources)
+
+    return 'https://twitter.com/search?q=({})%20({})%20-%22needed%22%20-%22need%22%20-%22needs%22%20-%22required%22%20-%22require%22%20-%22requires%22%20-%22requirement%22%20-%22requirements%22&f=live'.format(location_text, resources_text)
