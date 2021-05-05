2a9fe4998101d42e41d2166c444b987f846ac6b6
Archit Agrawal <archit18221@iiitd.ac.in>
2021-05-06 01:44:47 +0530

    Add current hospital scraping... with beautiful soup

--- src/tmp_get_hospitals/tmp.py
+++ src/tmp_get_hospitals/tmp.py
@@ -1,47 +1,33 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+import json
 import time
-options = Options()
-options.headless = True
-path_to_geckodriver = "E:\covid_bot\geckodriver.exe"
-sleep_time = 120
+import requests
+sleep_time = 60*5
 
-
-driver = webdriver.Firefox(executable_path=path_to_geckodriver, options=options)
+resources = ['beds', 'oxygen_beds', 'covid_icu_beds', 'ventilators', 'icu_beds_without_ventilator', 'noncovid_icu_beds']
 
 def fetch_details():
-    url = 'https://coronabeds.jantasamvad.org/all-covid-icu-beds.html'
+    url = 'https://coronabeds.jantasamvad.org/covid-info.js'
     #url =  'https://coronabeds.jantasamvad.org/beds.html'
-    driver.get(url)
-    print("Got data")
-    html = driver.page_source
-    soup = BeautifulSoup(html, features="html.parser")
-    info_list = soup.find('tbody',{ "id" : "hospitals_list" }).find_all('tr', {"class":"table-danger"})
-    hospital_info = {}
-    for row in info_list:
-        hospital_name = row.find('th').text.strip()
-        cols = row.find_all('td')
-        hospital_info[hospital_name] = {}
-        hospital_info[hospital_name]['update_time'] = cols[0].text.strip()
-        hospital_info[hospital_name]['beds_available'] = int(cols[2].text.strip().split(" ")[0])
-        #print("hospital_name", hospital_name, "update_time", update_time, "beds_available",beds_available)
-
-    return hospital_info
+    a = requests.get(url)
+    print("get data")
+    data = json.loads(a.content[23:-1])
+    for resource in resources:
+        del data[resource]["All"]
+    return data
 
 
-def compare_details(old_data, new_data):
-    for hospital_name, _ in old_data.items():
-        if new_data[hospital_name]['beds_available'] > old_data[hospital_name]['beds_available']:
-            print("GOT NEW ICU BED")
-            print(hospital_name, new_data[hospital_name])
+def find_resources(old_data, new_data):
+    for resource in resources:
+        for hospital in list(old_data[resource].keys()):
+            if new_data[resource][hospital]['vacant'] > old_data[resource][hospital]['vacant']:
+                print(hospital_name,"Got new",resource,  new_data[resource][hospital]['vacant'])
 
-
+            
 old_data = fetch_details()
-
+find_resources(old_data, old_data)
 while True:
     time.sleep(sleep_time)
     new_data = fetch_details()
-    print(new_data)
-    compare_details(old_data, new_data)
+    find_resources(old_data, new_data)
     old_data = new_data
