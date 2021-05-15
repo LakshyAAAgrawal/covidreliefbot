8fffb8c811757c5bdf3c6c64c67e399a59087928
Lakshya A Agrawal <lakshya.aagrawal@gmail.com>
2021-05-15 12:54:39 +0530

    Add resource

--- src/lists.py
+++ src/lists.py
@@ -1,1 +1,1 @@
-resource_name_list = ['oxygen', 'cylinder', 'ventilator', 'plasma', 'bed', 'icu', 'refill', 'ambulance', 'food', 'hospital', 'remdesivir', 'concentrator', 'beds', 'home icu', 'favipiravir', 'tocilizumab', 'fabiflu', 'test', 'tests', 'crematorium', 'baricitinib']
+resource_name_list = ['oxygen', 'cylinder', 'ventilator', 'plasma', 'bed', 'icu', 'refill', 'ambulance', 'food', 'hospital', 'remdesivir', 'concentrator', 'beds', 'home icu', 'favipiravir', 'tocilizumab', 'fabiflu', 'test', 'tests', 'crematorium', 'baricitinib', 'fungisome', 'phosome']
